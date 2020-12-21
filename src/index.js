df07370b455f2a5472ba03baef82e28645d441ae
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-21 16:50:02 -0300

    test route

--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ const app = Express();
 app.use(BodyParser.json())
 app.use(BodyParser.urlencoded({ extended: false }))
 
+app.get('/', (req, res) =>{
+
+    res.send('ok')
+})
+
+console.log('Servidor rodando em: http://localhost:3000/')
 app.listen(3000);

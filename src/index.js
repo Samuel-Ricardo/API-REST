e797b0fd4a0930db72ab6a4702e1b40a85f81bfa
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-21 16:40:02 -0300

    create app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import Express from 'express'
 import BodyParser from 'body-parse'
 
+const app = Express();
+
+app.use(BodyParser.json())
+app.use(BodyParser.urlencoded({ extended: false }))
+
+app.listen(3000);

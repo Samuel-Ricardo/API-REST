77903dff64f1ef7d639155e14dfe8de0341419f0
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-29 16:48:01 -0300

    Error message

--- src/Controller/AuthController.js
+++ src/Controller/AuthController.js
@@ -5,7 +5,15 @@ const User = require ('../models/User');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
+
+    const { email } = req.body;
+
     try {
+
+        if (await User.findOne({ email })){
+            return res.send(400).send({ error: 'User Already Exists'})
+        }
+        
         const user = await User.create(req.body);
     
         return res.send({ user })

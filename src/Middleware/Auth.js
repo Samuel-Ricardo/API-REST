e09e59c31dbd67fb0149d42745a8db9146c2b592
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-30 10:53:20 -0300

    Check: Error - 'Token Error'

--- src/Middleware/Auth.js
+++ src/Middleware/Auth.js
@@ -4,9 +4,14 @@ const AuthConfig = require('../Config/auth.json');
 
 module.exports = (req, res, next) => {
 
-    const AuthHeader = req.headers.Authorization
+    const authHeader = req.headers.Authorization
 
-    if(!AuthHeader)
+    if(!authHeader)
         return res.status(401).send({ error: 'No Token Provided'});
 
+
+        const parts = authHeader.split(" ")
+
+    if(! parts.length === 2)
+        return res.status(401).send({ error: 'Token Error'});
 }

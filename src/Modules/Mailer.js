625b194b874974206a16e02c658eace663b2dd48
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-30 11:48:43 -0300

    use handlebars

--- src/Modules/Mailer.js
+++ src/Modules/Mailer.js
@@ -1,12 +1,20 @@
 const nodemailer = require('nodemailer')
+const Path = require('path');
+const handlebars = require('nodemailer-express-handlebars');
 
-const {host, port, user, pass} = require('../Config/mail.json')
+const {host, port, user, pass} = require('../Config/mail.json');
 
 var transport = nodemailer.createTransport({
-    
+
     host,
     port,
     auth: { user, pass}
   });
+
+  transport.use('conpile', hbs({
+      viewEngine: 'handllebars',
+      viewPath: pathToFileURL.resolve('./src/Resources/Mail/'),
+      extName: '.html'
+  }));
 
   module.exports = transport;

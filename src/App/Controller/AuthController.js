138d41a7be34bf2c7cd7ee9658f262506e1603fc
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-30 12:05:06 -0300

    Catch error

--- src/App/Controller/AuthController.js
+++ src/App/Controller/AuthController.js
@@ -69,7 +69,14 @@ router.post('/authenticate', async (req, res) => {
 
 router.post('/forgot-password', async (req, res) => {
 
-        
+    const { email } = req.body;
+
+    try{
+
+    }catch(err){
+        return res.status(400).send({ error: 'Error: ' +err})
+    }
+
 })
 
 module.exports = app => app.use('/auth', router);

aa3d3f44b368a682f5302c2ab3cd85e38f1eadc7
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-30 10:58:19 -0300

    Check: error - 'Token Mal Formatado'

--- src/Middleware/Auth.js
+++ src/Middleware/Auth.js
@@ -10,8 +10,13 @@ module.exports = (req, res, next) => {
         return res.status(401).send({ error: 'No Token Provided'});
 
 
-        const parts = authHeader.split(" ")
+    const parts = authHeader.split(" ")
 
     if(! parts.length === 2)
         return res.status(401).send({ error: 'Token Error'});
+
+    const [ scheme, token] = parts
+
+    if(! /^Bearer$/i.test(scheme))
+        return res.status(401).send({ error: 'Token Mal Formatado'});
 }

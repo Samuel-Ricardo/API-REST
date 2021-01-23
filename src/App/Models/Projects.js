c86bfa5f718f609a476f951f3501bd33c34bffd9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-01-23 13:42:23 -0300

    create schema

--- src/App/Models/Projects.js
+++ src/App/Models/Projects.js
@@ -1,3 +1,31 @@
 const Mongoose = require("../../DataBase/Connection")
 
-const ProjectSchema;
+const ProjectSchema = Mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    
+    description: {
+        
+        type: String,
+        require: true
+    },
+
+    user: {
+
+        type: Mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    tasks: [{
+
+    }],
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+

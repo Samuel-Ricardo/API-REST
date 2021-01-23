b0c03846e07d01b265e2f799c21499a4ef607acb
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-01-23 14:45:13 -0300

    import project

--- src/App/Controller/ProjectController.js
+++ src/App/Controller/ProjectController.js
@@ -1,14 +1,20 @@
 const Express = require('express');
 
 const authMiddleware = require('../Middlewares/Auth');
+const Project = require('../Models/Project')
 
 const Router = Express.Router();
+
 
 Router.use(authMiddleware)
 
 Router.get('/', (req, res) => {
 
     res.send({ ok: true, user: req.userId })
+})
+
+Router.post('/create', (req, res) =>{
+    const
 })
 
 module.exports = app => app.use('/project', Router)
